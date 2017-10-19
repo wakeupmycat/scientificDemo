275b79ba6a03d4677b9c1d9d9f12a8d5037e1273
wakeupmycat <nxp_xinpeng@126.com>
2017-10-19 18:51:53 +0800

    header

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass',function () {
+    return gulp.src('./src/assets/css/*.scss')
+        .pipe(sass.sync().on('error',sass.logError))
+        .pipe(gulp.dest('./src/assets/css/'))
+});
+
+gulp.task('sass:watch',function () {
+    gulp.watch('./src/assets/css/*.scss',['sass']);
+})
